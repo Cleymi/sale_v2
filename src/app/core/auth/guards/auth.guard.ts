62fe6ae001a78fe7bc40af5cff6d4cb7331fcf33
Clever <clever.coer09@gmail.com>
2023-03-23 20:23:04 -0500

    ajuste al topbar

--- src/app/core/auth/guards/auth.guard.ts
+++ src/app/core/auth/guards/auth.guard.ts
@@ -2,19 +2,17 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  authService: any = localStorage.getItem("token") //TODO sacar el token del localStorage
+  authService: any = localStorage.getItem('token'); //TODO sacar el token del localStorage
 
   constructor(
     /* private authService: AuthService, */
-    private router: Router
-  ) { }
+    private router: Router,
+  ) {}
 
   canActivate() {
-    return this.authService !== null ? true : this.router.navigate(['auth/login'])
+    return this.authService !== null ? true : this.router.navigate(['auth/login']);
   }
-
 }
